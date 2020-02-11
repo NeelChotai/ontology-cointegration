1af2e2a3a7c83cdbadfa0b23f4b4f4c9b75802c7
Neel <neel@neelchotai.com>
2020-02-11 15:28:16 +0000

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dissertation
+
+Project files for third year project.
+
+Useful links:
+- Python library for SPARQL: https://rdflib.github.io/sparqlwrapper/
+- Company directorship: https://1drv.ms/u/s!Agm4tjbFzC37j9R8GGmruC1-JlICTA?e=EfgspE
+- Stock data: https://1drv.ms/u/s!Agm4tjbFzC37j9R9nAAmpKpl-VKveQ?e=gwoYFh
---
+++ src/main.py
@@ -0,0 +1,16 @@
+def main():
+    # after reading in ontology data, create a sorted list descending by highest cointegration factor
+    # performance can be returned individually if user requests or generate graphs
+    print("Hello world.")
+
+def generate():
+    # generate and sort SPARQL queries here
+    # return as CSV?
+    # return format: {} where key = ticker pair and 
+    print("todo")
+
+def evaluate(ticker, days):
+    # read in ticker, output percentage increase within x days (optional)
+    # if days not specified, presume entire timeline
+    # return percentage increase
+    print(todo)
