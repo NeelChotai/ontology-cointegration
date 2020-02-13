daa9b480c0eff80b20efe8ffb660cbfc09aa1935
Neel <neel@neelchotai.com>
2020-02-13 23:01:08 +0000

    parser planning

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from pandas import read_csv
+
 def main():
     # after reading in ontology data, create a sorted list descending by highest cointegration factor
     # performance can be returned individually if user requests or generate graphs
@@ -6,11 +8,19 @@ def main():
 def generate():
     # generate and sort SPARQL queries here
     # return as CSV?
-    # return format: {} where key = ticker pair and 
+    # return format: dictionary where key = ticker pair and cointegration score
     print("todo")
 
-def evaluate(ticker, days):
-    # read in ticker, output percentage increase within x days (optional)
-    # if days not specified, presume entire timeline
-    # return percentage increase
-    print(todo)
+def evaluate(ticker):
+    # read in ticker, output percentage increase within one month, 6 months and entire timeline
+    # return percentage increase or some metric of performance
+    data = read_csv("data/tickers/" + ticker + ".csv").sort_values("Date", ignore_index=True) # ticker value sorted by date
+    close_price = data.filter(["Close"])
+    one_month = ((close_price.loc[30] - close_price.loc[0])/close_price.loc[0]) * 100
+    six_months = ((close_price.loc[182] - close_price.loc[0])/close_price.loc[0]) * 100
+    timeline = ((close_price.loc[len(data) - 1] - close_price.loc[0])/close_price.loc[0]) * 100
+
+    return float(one_month), float(six_months), float(timeline)
+    
+
+print(evaluate("INTC"))
