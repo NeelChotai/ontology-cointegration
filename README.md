9d39324a931d94b46828f8f46ea11e69e1d784d7
Neel Chotai <neel@chot.ai>
2020-05-10 04:41:00 +0100

    updated completed tasks, switched from CSV stock data to on-demand

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 Project files for third year project.
 
-Current task: preprocessing
+Current tasks: 
+- Generating SPARQL to return high attribute set.
+- Selecting range of stock data to use.
+
+Completed tasks:
+
+- Inputting ontology.
+- Cointegration with Engle-Granger and Johansen.
+- Control set (random pair) selection.
--- src/main.py
+++ src/main.py
@@ -1,45 +1,89 @@
-from pandas import read_csv
+import pandas as pd
+import numpy as np
 import rdflib
 import os
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import coint
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
+import random
+import pickle
+import yfinance as yf
 
-def preprocess(graph):
-    #remove entries for which we don't have ticker data
-    tickers = []
-
-    for filename in os.listdir("data/tickers/"):
-        ticker = filename.split(".")
-        tickers.append(ticker[0])
-
-    # scan through ontology, check if each ticker value exists in data
-    # if not, remove entry
-    # return transformed graph
-
-    return graph
+cache_file = "/tmp/graph.cache"
 
 def populate():
     graph = rdflib.Graph()
 
     for x in range(0, 29):
-        temp_graph = rdflib.Graph().parse("data/directorship/ownership-" + str(x) + ".nt", format="nt")
+        temp_graph = rdflib.Graph().parse("data/directorship/ownership-{}.nt".format(str(x)), format="nt")
         graph += temp_graph
 
-    return preprocess(graph)
+    return graph
 
-def generate():
-    # generate and sort SPARQL queries here
-    # return as CSV?
+def initial_run(cache_file):
+    # for ease of programming
+    # the graph data structure is enormous, pickling prevents regenerating the same structure every run
+
+    if os.path.isfile(cache_file):
+        infile = open(cache_file, "rb")
+        graph = pickle.load(infile)
+        infile.close()
+    else:
+        graph = populate()
+        outfile = open(cache_file, "wb")
+        pickle.dump(graph, outfile)
+        outfile.close()
+
+def random_companies(sample_size):
+    random_out = []
+
+    with open("stocks.txt") as stocks:
+        tickers = stocks.read().split(",")
+    
+    for x in range(sample_size):
+        pair = (random.choice(tickers), random.choice(tickers))
+        
+        if pair[0] == pair[1] or pair in random_out or reversed(pair) in random_out:
+            sample_size += 1
+        else:
+            random_out.append(pair)
+
+    return random_out
+
+def generate(graph):
+    # SPARQL queries here
     # return format: dictionary where key = ticker pair and number of shared attributes
-    print("todo")
+    # if sample size given, return subdict descending
 
-def evaluate(ticker):
-    # read in ticker, output percentage increase within one month, 6 months and entire timeline
-    # return percentage increase or some metric of performance
-    data = read_csv("data/tickers/" + ticker + ".csv").sort_values("Date", ignore_index=True) # ticker value sorted by date
-    close_price = data.filter(["Close"])
-    one_month = ((close_price.loc[30] - close_price.loc[0])/close_price.loc[0]) * 100
-    six_months = ((close_price.loc[182] - close_price.loc[0])/close_price.loc[0]) * 100
-    timeline = ((close_price.loc[len(data) - 1] - close_price.loc[0])/close_price.loc[0]) * 100
+    shared_attributes = graph.query(
+        ''' some query
+        ''')
+    
+    return shared_attributes
 
-    return float(one_month), float(six_months), float(timeline)
+def cointegrate(ticker1, ticker2):
+    # cointegrates two time series given by tickers
+    # returns True if time series are cointegrated
 
-graph = populate()
+    series1 = yf.download(ticker1, "2015-01-01", "2020-01-01").filter(["Date", "Close"])
+    series2 = yf.download(ticker2, "2015-01-01", "2020-01-01").filter(["Date", "Close"])
+    
+    merged = pd.merge(series1, series2, how="outer", on=["Date"])
+    merged.dropna(inplace=True)
+    print(merged)
+    johansen_frame = pd.DataFrame({"x":merged["Close_x"], "y":merged["Close_y"]})
+
+    score, p_value, _ = coint(merged["Close_x"], merged["Close_y"])
+    johansen = coint_johansen(johansen_frame, 0, 1)
+
+    if p_value < 0.05 or johansen.cvt[1][0] < johansen.lr1[1]: # calculates cointegration using p-value and trace statistic (90% confidence)
+        return True
+    return False
+
+#test = random_companies(100)
+
+#import sys
+#for pair in test:
+#    sys.stdout.write("{}/{}, ".format(pair[0], pair[1]))
+
+p = cointegrate("GOOG", "GOOGL")
