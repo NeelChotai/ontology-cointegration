49f079d4fdcd6d05c71c96a2479a705d1fc74a56
Neel Chotai <neel@chot.ai>
2020-08-08 01:00:10 +0100

    interval testing

---
+++ intervals.py
@@ -0,0 +1,17 @@
+from main import *
+
+if path.isfile(GRAPH_CACHE):
+    graph = pop_cache(GRAPH_CACHE)
+else:
+    graph = populate()
+    push_cache(GRAPH_CACHE, graph)
+
+employee_dict = pairs_with_attributes(query(graph, employee_type.EMPLOYEE))
+return_dict = {}
+
+for interval in [1, 2, 3, 4, 5]:
+    employee_pairs = [x for x in list(
+        employee_dict) if employee_dict[x] == interval]
+    return_dict[interval] = len(employee_pairs)
+
+print(return_dict)
