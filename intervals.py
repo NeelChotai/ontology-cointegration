b08f69d9829a8fdbdabbeb300ada63e002988d15
Neel Chotai <neel@chot.ai>
2020-08-10 16:50:55 +0100

    change interval count to reflect chosen selection method

--- intervals.py
+++ intervals.py
@@ -9,9 +9,9 @@ else:
 employee_dict = pairs_with_attributes(query(graph, employee_type.EMPLOYEE))
 return_dict = {}
 
-for interval in [1, 2, 3, 4, 5]:
+for interval in range(1, 6):
     employee_pairs = [x for x in list(
-        employee_dict) if employee_dict[x] == interval]
+        employee_dict) if employee_dict[x] >= interval]
     return_dict[interval] = len(employee_pairs)
 
 print(return_dict)
